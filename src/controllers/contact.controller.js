a5140dbbcdde75700595bc11ba0c0725eb206fd6
Muhammad Umar Farooq <develop.umar@gmail.com>
2023-03-15 12:20:59 +0500

    controllers

---
+++ src/controllers/contact.controller.js
@@ -0,0 +1,27 @@
+const getContacts = () => {
+    // get contacts logic will go here...
+}
+
+const createContact = () => {
+    // create contact logic will go here...
+}
+
+const getContact = () => {
+    // get single contact logic will go here...
+}
+
+const updateContact = () => {
+    // update contact logic will go here...
+}
+
+const deleteContact = () => {
+    // delete contact logic will go here...
+}
+
+module.exports = {
+    getContacts,
+    createContact,
+    getContact,
+    updateContact,
+    deleteContact
+}
--- src/routes/contact.routes.js
+++ src/routes/contact.routes.js
@@ -1,22 +1,31 @@
 const express = require('express');
+const {getContacts, createContact, getContact, updateContact, deleteContact} = require("../controllers/contact.controller");
 const router = express.Router();
 
 router.get('/contacts', (req, res) => {
+    getContacts();
     res.status(200).json({message: 'Get contacts'});
 });
 
 router.post('/contacts', (req, res) => {
+    createContact();
     res.status(200).json({message: 'Create contacts'});
 });
 
+router.get('/contacts/:id', (req, res) => {
+    getContact();
+    res.status(200).json({message: `Get contact with id ${req.params.id}`});
+});
+
 router.put('/contacts/:id', (req, res) => {
+    updateContact();
     res.status(200).json({message: `Update contact with id ${req.params.id}`});
 });
 
 router.delete('/contacts/:id', (req, res) => {
+    deleteContact();
     res.status(200).json({message: `Remove contact with id ${req.params.id}`});
 });
-
 
 
 module.exports = router;
