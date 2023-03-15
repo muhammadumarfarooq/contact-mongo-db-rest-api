d860a1d715f832496c39c023233cea9ed4e55d6f
Muhammad Umar Farooq <develop.umar@gmail.com>
2023-03-15 11:55:00 +0500

    http methods with express routes

--- src/routes/contact.routes.js
+++ src/routes/contact.routes.js
@@ -2,7 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/contacts', (req, res) => {
-    res.status(200).json({message: 'Message will go here...'});
+    res.status(200).json({message: 'Get contacts'});
 });
+
+router.post('/contacts', (req, res) => {
+    res.status(200).json({message: 'Create contacts'});
+});
+
+router.put('/contacts/:id', (req, res) => {
+    res.status(200).json({message: `Update contact with id ${req.params.id}`});
+});
+
+router.delete('/contacts/:id', (req, res) => {
+    res.status(200).json({message: `Remove contact with id ${req.params.id}`});
+});
+
+
 
 module.exports = router;
